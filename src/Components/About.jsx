69dc8fc9cb549b6f37355aa820179c760bb3413a
qistina <134914604+rsmnrp@users.noreply.github.com>
2024-05-01 10:43:12 +0000

    Pending changes exported from your codespace

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,24 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/nz.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "an image of the mountains in New Zealand";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm an Economics and Data Science student studying at Singapore Management University. I enjoy challenging myself and trying out new things.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "R",
+  "Python",
+  "Apache Spark",
 ];
 
 /**
@@ -45,7 +42,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate in leveraging on my analytical skills to improve decision-making processes.";
 
 const About = () => {
   return (
